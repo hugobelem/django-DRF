8b6e0d1cee06dcc194604c0785f2e072fa0fafb5
Hugo Belém <belem.hugo@hotmail.com>
2024-09-06 19:41:16 -0300

    new endpoint

--- api/urls.py
+++ api/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('books/', views.BooksListCreate.as_view(), name='books'),
+    path('books/<int:pk>/',
+         views.BooksRetieveUpdateDestroy.as_view(),
+         name='update'),
 ]
