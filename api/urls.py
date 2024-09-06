b7fdea362f210efb6cc247c5f6e5f0e15eb703e6
Hugo Belém <belem.hugo@hotmail.com>
2024-09-06 19:15:48 -0300

    deleted old paths

--- api/urls.py
+++ api/urls.py
@@ -4,6 +4,4 @@ from . import views
 
 urlpatterns = [
     path('books/', views.BooksListCreate.as_view(), name='books'),
-    path('books/add', views.add_book, name='add_book'),
-    path('books/<int:pk>/', views.book_detail, name='book_detail'),
 ]
