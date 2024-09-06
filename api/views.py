11d9b913cfc9bd0e20c7c105e3eebeb680ea5b0b
Hugo Belém <belem.hugo@hotmail.com>
2024-09-06 20:13:23 -0300

    new custom api view

--- api/views.py
+++ api/views.py
@@ -1,6 +1,6 @@
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
 from rest_framework import status, generics
+from rest_framework.views import APIView
 
 from .models import Books
 from .serializers import BooksSerializer
@@ -16,3 +16,14 @@ class BooksRetieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = BooksSerializer
     lookup_field = 'pk'
 
+class BooksAuthor(APIView):
+    def get(self, request, format=None):
+        author = request.query_params.get('author', '')
+
+        if author:
+            book = Books.objects.filter(author__icontains=author)
+        else:
+            book = Books.objects.all()
+
+        serializer = BooksSerializer(book, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
